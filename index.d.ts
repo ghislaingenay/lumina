28bcfa9e8459ab7efc7d2ba9786902980eb02e6f
Ghislain Genay <103702791+ghislaingenay@users.noreply.github.com>
2026-03-15 16:53:51 +0700

    add base files

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+import type { ThreeToJSXElements } from "@react-three/fiber";
+
+declare module "@react-three/fiber" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface ThreeElements extends ThreeToJSXElements<typeof THREE> {}
+}
