fe1f13e5b8ca196e6cce20947dc474a96c922c44
ghislaingenay <ghislain.gny@gmail.com>
2026-03-19 22:11:31 +0700

    feat: add experimental optimizePackageImports option to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   /* config options here */
+  experimental: {
+    // If @components resolves to an index.ts barrel, the "use client" directive on landscape_overlay.tsx
+    // Can cause it to be registered in both the server and client module graphs, making the name appear twice to the SWC compiler.
+    optimizePackageImports: ["@components"],
+  },
 };
 
 export default nextConfig;
