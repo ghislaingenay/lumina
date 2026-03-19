d00afaea6c08d51ba852e2f30ce3b795ecb4613a
ghislaingenay <ghislain.gny@gmail.com>
2026-03-19 22:35:11 +0700

    feat: add LandscapeOverlay component to ConfiguratorModule and LandingModule

--- modules/configurator/index.tsx
+++ modules/configurator/index.tsx
@@ -1,3 +1,4 @@
+import LandscapeOverlay from "@components/landscape_overlay";
 import ConfiguratorLoading from "./components/configurator_loading";
 import ReturnToHome from "./components/return_to_home";
 import ConfiguratorSidebar from "./components/sidebar";
@@ -6,6 +7,8 @@ import ConfiguratorExperience from "./configurator_experience";
 export default function ConfiguratorModule() {
   return (
     <div className="relative h-screen w-full">
+      <LandscapeOverlay />
+
       <div className="absolute size-full p-4 top-0 left-0 z-50 pointer-events-none">
         <ReturnToHome />
       </div>
